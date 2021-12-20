05cdeda864153badfb427d1070ffe274d527295c
Chris <ucabilk@ucl.ac.uk>
2021-12-20 21:29:07 +0000

    added data processing in init function of class

--- data_pipeline/DataUtils.py
+++ data_pipeline/DataUtils.py
@@ -2,28 +2,41 @@ import numpy as np
 import pandas as pd
 import h5py as h5
 import os
-'''
-helper functions for loading in data for the dataset class (merged)
-''' 
-cur_path = os.path.dirname(__file__)
-print(cur_path)
-PATH = cur_path[:-14] #this is absolutely disgusting, dont judge (removes /data_pipeline from the directory)
-PATH = f'{PATH}/datasets'
 
+PATH = f'{os.path.dirname(__file__)[:-15]}/datasets'
+print(PATH)
+HEIGHT = 256
+WIDTH = 256
 
 def paths(folder, file):
     '''
-    Takes the route director and adds folder and file name
+    Takes the route directory and adds folder and file name
     '''
     p = f'{PATH}/{folder}/{file}'
     return p
 
 def load_data_from_h5(path, inds):
+    '''
+    opens h5 file and returns contents (shape = 256x256x3)
+    (these files all only have one key)
+    '''
     with h5.File(path, 'r') as file:
         key = list(file.keys())[0]
         elems = file[key][inds]
     return elems
 
-p = paths('test', 'images.h5')
-print(p)
-f = h5.File(p)
+TEST = {'images': load_data_from_h5(paths('test', 'images.h5')), 
+        'bboxes': load_data_from_h5(paths('test', 'bboxes.h5')), 
+        'binary': load_data_from_h5(paths('test', 'binary.h5')),
+        'masks': load_data_from_h5(paths('test', 'masks.h5'))}
+        
+TRAIN = {'images': load_data_from_h5(paths('train', 'images.h5')), 
+        'bboxes': load_data_from_h5(paths('train', 'bboxes.h5')), 
+        'binary': load_data_from_h5(paths('train', 'binary.h5')),
+        'masks': load_data_from_h5(paths('train', 'masks.h5'))}
+        
+VAL = {'images': load_data_from_h5(paths('val', 'images.h5')), 
+        'bboxes': load_data_from_h5(paths('val', 'bboxes.h5')), 
+        'binary': load_data_from_h5(paths('val', 'binary.h5')),
+        'masks': load_data_from_h5(paths('val', 'masks.h5'))}
+
--- data_pipeline/DatasetClass.py
+++ data_pipeline/DatasetClass.py
@@ -4,12 +4,15 @@ Created 19:15 21/12/2021 by Christopher Wilkin
 Dataloader file containing all methods and functions relating to loading data...
 '''
 
+from genericpath import sameopenfile
 import h5py
 import os
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
+import DataUtils
+from DataUtils import TEST, TRAIN, VAL
 
 h5_save_path = r"datasets/"
 num_samples = 256
@@ -17,12 +20,35 @@ num_samples = 256
 
 
 class PetSegmentationDataSet(Dataset):
-    def __init__(self, path, image=True, mask=True, bbox=True, bin=True):
+    def __init__(self, data, mask=True, bbox=True, bin=True):
+        '''
+        Data: Dictionary of Images, BBoxes, Binary, Masks
+        mask: whether to include mask data in dataset
+        bbox: whether to include bbox data in dateset
+        bin: whether to unclude binary data in dataset
+        '''
         super().__init__()
+        self.mask = mask
+        self.bbox = bbox
+        self.bin = bin
+        self.data = data
+        if not self.mask:
+            self.data = self.data.pop('masks', None)
+        if not self.bbox:
+            self.data = self.data.pop('bboxes', None)
+        if not self.bin:
+            self.data = self.data.pop('binary', None)
+        
+        for key in self.data.keys():
+            self.data[key] = torch.tensor(self.data[key])
+
 
     def __len__(self):
-        return super().__len__()
+        return len(self.data['images'])
 
-    def __getitem__(self, index: int):
-        return super().__getitem__(index)
+    def __getitem__(self, idx):
+        
+
+
+        return sample
     
