3e74261ba35592c4efd8d2b3e39756e488780633
Chris <ucabilk@ucl.ac.uk>
2021-12-22 22:19:18 +0000

    implemented image folder loading. Takes 40mins to load all images wth no processing.

--- data_pipeline/DataUtils.py
+++ data_pipeline/DataUtils.py
@@ -41,35 +41,58 @@ def array_from_jpg(file_name):
     Opens a .jpg file and returns a numpy array of H x W x C
     '''
     file = Image.open(file_name)
-    pix = np.array(file.getdata()).reshape(file.size[0], file.size[1], 3)
+    try:
+        pix = np.array(file.getdata()).reshape(file.size[0], file.size[1], 3)
+    except:
+        print(f'Failed to convert {file_name} to array!')
+        pix = []
     file.close()
     return pix
 
 #p = paths(PATH_OG, 'images', 'Abyssinian_1.jpg')
 #print(array_from_jpg(p))
 
-def load_data(data, test_train_val=None, indices=None):
+def load_data(folder, test_train_val=None, indices=None):
     '''
     data: images / masks / bboxes / bins - specifies the type of data to be loaded
     test_train_val: if set to 'train' or 'test' or 'val, uses a default 60:20:20 data split. Default None returns all data
     indices: if None, all data returned, otherwise accepts int or array([int]) to return specific indices of data
     '''
-    assert data in ['images', 'masks', 'bboxes', 'bins'], 'Invalid data category. Must be in images / masks / bboxes / bins'
+    assert folder in ['images', 'masks', 'bboxes', 'bins'], 'Invalid data category. Must be in images / masks / bboxes / bins'
     
-    if test_train_val != None:
-        assert indices == None, 'test_train_val and indices cannot both be specified'
+    if test_train_val is not None:
+        assert indices is None, 'test_train_val and indices cannot both be specified'
         assert test_train_val in ['test', 'train', 'val'], 'Invalid argument. Must be in test / train / val'
 
-    elif indices != None:
-        assert indices is np.ndarray, 'Invalid indices entry. Must by numpy array of integers'
+    elif indices is not None:
+        assert isinstance(ind, np.ndarray), 'Invalid indices entry. Must by numpy array of integers'
+        indices = indices.tolist()
 
-    if data == 'images':
+    data = []
+
+    if folder == 'images':
         path = paths(PATH_OG, 'images')
-    elif data == 'masks':
+        file_names = np.array(next(os.walk(path), (None, None, []))[2])  # [] if no file
+        jpgs = [file_names[i][-3:] == 'jpg' for i in range(len(file_names))] #ignore any non .jpg files
+        file_names = file_names[jpgs]
+        if indices is not None:
+            file_names = [file_names[i] for i in indices]
+        for i, name in enumerate(file_names):
+            data.append(array_from_jpg(paths(path, name)))
+            if i+1 % 250 == 0:
+                print(f'{i+1} / {len(file_names)}')
+
+    elif folder == 'masks':
         path = paths(PATH_OG, 'annotations', 'trimaps')
-    elif data == 'bboxes':
+    elif folder == 'bboxes':
         path = paths(PATH_OG, 'annotations', 'xmls')
-    elif data == 'bins':
+    elif folder == 'bins':
         path = paths(PATH_OG, 'annotations')
 
-    return
+    return data
+
+
+ind = np.arange(1, 10, 1)
+assert isinstance(ind, np.ndarray)
+x = load_data('images', indices=ind)
+print(x[0])
