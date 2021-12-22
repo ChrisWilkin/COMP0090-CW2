7f3505783883e8eedb4ecc19e1e68e12ea9eaaa7
Chris <ucabilk@ucl.ac.uk>
2021-12-22 21:03:49 +0000

    made paths function more general

--- data_pipeline/DataUtils.py
+++ data_pipeline/DataUtils.py
@@ -1,28 +1,34 @@
 import h5py as h5
 import os
 
-PATH = f'{os.path.dirname(__file__)[:-14]}/datasets'
+PATH = f'{os.path.dirname(__file__)[:-14]}/Datasets'
 HEIGHT = 256
 WIDTH = 256
 
-def paths(folder, file):
+def paths(path, *args):
     '''
     Takes the route directory and adds folder and file name
     '''
-    p = f'{PATH}/{folder}/{file}'
-    return p
+    if len(args) == 0:
+        return path 
+    for a in args:
+        path = f'{path}/{a}'
+    return path
 
 def load_data_from_h5(folder, file):
     '''
     opens h5 file and returns contents (shape = 256x256x3)
     (these files all only have one key)
     '''
-    path = paths(folder, file)
+    path = paths(PATH, folder, file)
     with h5.File(path, 'r') as file:
         key = list(file.keys())[0]
         elems = file.get(key)[:]
 
     return elems
 
+
+
+PATH_OG = f'{os.path.dirname(__file__)[:-14]}/Datasets/CompleteDataset'
 
 
