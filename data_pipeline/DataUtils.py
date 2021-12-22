9442b67b13880a1d3e18b25db8e60115d34947fb
Chris <ucabilk@ucl.ac.uk>
2021-12-22 21:42:22 +0000

    added load_data function with paths to relevent folders

--- data_pipeline/DataUtils.py
+++ data_pipeline/DataUtils.py
@@ -1,5 +1,10 @@
 import h5py as h5
 import os
+import numpy as np
+import PIL
+from PIL import Image
+
+from numpy.testing._private.utils import print_assert_equal
 
 PATH = f'{os.path.dirname(__file__)[:-14]}/Datasets'
 HEIGHT = 256
@@ -27,8 +32,44 @@ def load_data_from_h5(folder, file):
 
     return elems
 
-
+##### New Code to process orignal data
 
 PATH_OG = f'{os.path.dirname(__file__)[:-14]}/Datasets/CompleteDataset'
 
+def array_from_jpg(file_name):
+    '''
+    Opens a .jpg file and returns a numpy array of H x W x C
+    '''
+    file = Image.open(file_name)
+    pix = np.array(file.getdata()).reshape(file.size[0], file.size[1], 3)
+    file.close()
+    return pix
 
+#p = paths(PATH_OG, 'images', 'Abyssinian_1.jpg')
+#print(array_from_jpg(p))
+
+def load_data(data, test_train_val=None, indices=None):
+    '''
+    data: images / masks / bboxes / bins - specifies the type of data to be loaded
+    test_train_val: if set to 'train' or 'test' or 'val, uses a default 60:20:20 data split. Default None returns all data
+    indices: if None, all data returned, otherwise accepts int or array([int]) to return specific indices of data
+    '''
+    assert data in ['images', 'masks', 'bboxes', 'bins'], 'Invalid data category. Must be in images / masks / bboxes / bins'
+    
+    if test_train_val != None:
+        assert indices == None, 'test_train_val and indices cannot both be specified'
+        assert test_train_val in ['test', 'train', 'val'], 'Invalid argument. Must be in test / train / val'
+
+    elif indices != None:
+        assert indices is np.ndarray, 'Invalid indices entry. Must by numpy array of integers'
+
+    if data == 'images':
+        path = paths(PATH_OG, 'images')
+    elif data == 'masks':
+        path = paths(PATH_OG, 'annotations', 'trimaps')
+    elif data == 'bboxes':
+        path = paths(PATH_OG, 'annotations', 'xmls')
+    elif data == 'bins':
+        path = paths(PATH_OG, 'annotations')
+
+    return
