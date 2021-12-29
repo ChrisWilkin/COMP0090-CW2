9abb742ee173ee0213d0b83566612810061faf54
Chris <ucabilk@ucl.ac.uk>
2021-12-29 17:46:05 +0000

    Created working network. Next step create forward pass and thresholding

--- Coursework2/networks/YOLO.py
+++ Coursework2/networks/YOLO.py
@@ -18,6 +18,9 @@ class DetectionLayer(nn.Module):
         self.anchors = anchors 
 
 class Skip(nn.Module):
+    '''
+    Used as an empty layer
+    '''
     def __init__(self):
         super(Skip, self).__init__()
 
@@ -44,25 +47,28 @@ def generate_conv_blocks(feat1, feat2, n, ind):
         block.add_module(f'conv1x1_{i + ind}', nn.Conv2d(input_1 if i == 0 else input_2, feat1, 1, 1, 0))
         block.add_module(f'BN_{i + ind}', nn.BatchNorm2d(feat1))
         block.add_module(f'Leaky_{i + ind}', nn.LeakyReLU())
-        blocks.add_module(module=block)
+        blocks.add_module('block', module=block)
         filters.append(feat1)
         
         block = nn.Sequential()
         block.add_module(f'conv3x3_{i + ind}', nn.Conv2d(feat1, feat2, 3, 1, 1))
         block.add_module(f'BN_{i + ind}', nn.BatchNorm2d(feat2))
         block.add_module(f'Leaky_{i + ind}', nn.LeakyReLU())
-        blocks.add_module(module=block)
+        blocks.add_module('block', module=block)
         filters.append(feat2)
-
-        blocks.add_module(f'Shortcut_{i + ind}', Skip)
+        
+        blocks.add_module(f'Shortcut_{i + ind}', Skip())
         filters.append(2 * feat2)
     
     return blocks, np.array(filters)
 
 def generate_pool_blocks(feat1, feat2, mask):
+    '''
+    feat1 - number of filters outputted by 1x1 conv
+    feat2 - number of filters outputted by 3x3 conv
+    mask - mask indices for detection layer
+    '''
     blocks = nn.ModuleList()
-    input_1 = feat2
-    input_2 = 2 * feat2
     filters = []
     anchors = [[10,13], [16,30], [33,23], [30,61], [62,45], [59,119], [116,90], [156,198], [373,326]]
 
@@ -71,31 +77,35 @@ def generate_pool_blocks(feat1, feat2, mask):
         block.add_module(f'conv1x1_{i}', nn.Conv2d(feat2 if i != 0 else 2 * feat2, feat1, 1, 1, 0))
         block.add_module(f'BN_{i}', nn.BatchNorm2d(feat1))
         block.add_module(f'LeakyReLU', nn.LeakyReLU())
-        blocks.add_module(module=block)
+        blocks.add_module('block', module=block)
+        filters.append(feat1)
 
         block = nn.Sequential()
         block.add_module(f'conv3x3_{i}', nn.Conv2d(feat1, feat2, 3, 1, 1))
         block.add_module(f'BN_{i}', nn.BatchNorm2d(feat2))
         block.add_module(f'LeakyReLU', nn.LeakyReLU())
-        blocks.add_module(module=block)
+        blocks.add_module('block', module=block)
+        filters.append(feat2)
 
     block = nn.Sequential()
-    block.add_module(f'conv1x1_{i}', nn.Conv2d(feat2, (feat1 / 2) - 1, 1, 1, 0))
+    block.add_module(f'conv1x1_{i}', nn.Conv2d(feat2, int(feat1 / 2) - 1, 1, 1, 0))
     block.add_module(f'ReLU', nn.ReLU())
-    blocks.add_module(module=block)   
+    blocks.add_module('block', module=block)   
+    filters.append((feat1 / 2) - 1)
 
 
     # YOLO layer
-    a = []
-    for mask in ma
+    anchors = [anchors[i] for i in mask]
+    detection = DetectionLayer(anchors)
+    blocks.add_module(f'Detection_{i}', detection)
         
-    return blocks
+    return blocks, filters
     
 
 class YOLO(nn.Module):
     def __init__(self):
         super(YOLO, self).__init__()
-        self.filters = np.array([])
+        self.filters = np.array([0])
         
         # intial convolutions
         self.features = nn.ModuleList()
@@ -103,31 +113,39 @@ class YOLO(nn.Module):
         block.add_module('conv_0', nn.Conv2d(3, 32, 3, stride=1, padding=1, bias=False))
         block.add_module('batch_norm_0', nn.BatchNorm2d(32))
         block.add_module('leaky_0', nn.LeakyReLU())
-        self.features.add_module(module=block)
+        self.features.add_module('block', module=block)
 
         for i, n in enumerate([1,2,8,8,4]):
             inputs = 64 * (2**n)
             #BLOCK 
-            blocks, f = generate_conv_blocks(inputs, 64, n)
-            self.filters = np.concatenate((self.features, f))
+            blocks, f = generate_conv_blocks(inputs, 64, n, i)
+            self.filters = np.concatenate((self.filters, f))
             self.features.extend(blocks)
 
             block = nn.Sequential()
-            block.add_module(f'conv_{i+1}', nn.Conv2d(inputs if n != 1 else inputs/2, 64, 3 stride=2, padding=1, bias=False))
+            block.add_module(f'conv_{i+1}', nn.Conv2d(inputs if n != 1 else int(inputs/2), 64, 3, stride=2, padding=1, bias=False))
             block.add_module(f'batch_norm_{i+1}', nn.BatchNorm2d(364))
             block.add_module(f'leaky_{i+1}', nn.LeakyReLU())
-            self.features.add_module(module=block)
+            self.features.add_module('block', module=block)
 
         ########### AvgPool - Connected - Softmax ##############
 
             
+        for i, mask in enumerate([[6,7,8], [3,4,5], [0,1,2]]):
 
+            blocks, f = generate_pool_blocks(128 * (2 ** (2-i)), 256 * (2 ** (2-i)), mask)
+            self.filters = np.concatenate((self.features, f))
+            self.features.extend(blocks)
 
-        
-
+            if i != 2:
+                feat1 = 128 * (2 ** (2-i)) - 1
+                self.features.add_module(f'Route_{i}', Skip())
+                self.features.add_module(f'Upsample_{i}', nn.Upsample(scale_factor=2, mode='bilinear'))
+                self.features.add_module(f'Conv1x1_{i}', nn.Conv2d(int(feat1 / 2) - 1, int(feat1/2), 1, 1, 0))
+                self.features.add_module(f'Route_{i}', Skip())
+            
     def forward(self, x):
-        for item in self.blocks:
-            pass
+        
         return x
 
-
+net = YOLO()
