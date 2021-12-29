bb9bba5c2cd480bbb741783d6b9d2b85584d3358
Chris <ucabilk@ucl.ac.uk>
2021-12-29 23:12:51 +0000

    continue sorting out size issues

--- Coursework2/networks/YOLO.py
+++ Coursework2/networks/YOLO.py
@@ -1,33 +1,62 @@
 import torch
 import torch.nn as nn
-from torch.nn import modules
-import torch.nn.functional as F
-from torch.nn.modules import module
-from torch.nn.modules.activation import ReLU
-from torch.nn.modules.batchnorm import BatchNorm2d
-from torch.nn.modules.conv import Conv2d
+from torch.autograd import Variable
 import sys
 import os
 import numpy as np
+from PIL import Image
+
 sys.path.append(os.path.dirname(__file__)[:-len('/networks')]) #Import other folders after this line
+
+def get_test_input():
+    img = Image.open("dog-cycle-car.png", 'r')
+    img = np.asarray(img.resize((256,256)))         #Resize to the input dimension
+    img_ =  img[:,:,::-1].transpose((2,0,1))  # BGR -> RGB | H X W C -> C X H X W 
+    img_ = img_[np.newaxis,:,:,:]/255.0       #Add a channel at 0 (for batch) | Normalise
+    img_ = torch.from_numpy(img_).float()     #Convert to float
+    img_ = Variable(img_)                     # Convert to Variable
+    return img_
+    
+
 
 
 class DetectionLayer(nn.Module):
     def __init__(self, anchors):
         super(DetectionLayer, self).__init__()
-        self.anchors = anchors 
+        self.anchor = anchors 
+
+    def anchors(self):
+        return self.anchor
 
 class Skip(nn.Module):
     '''
     Used as an empty layer
     '''
-    def __init__(self):
+    def __init__(self, skip):
         super(Skip, self).__init__()
+        self.skip = skip
 
     def forward(self, x):
         return x
 
-def generate_conv_blocks(feat1, feat2, n, ind):
+    def skips(self):
+        return self.skip
+
+class Route(nn.Module):
+    '''
+    Used as an empty layer
+    '''
+    def __init__(self, layer):
+        super(Route, self).__init__()
+        self.layer = layer
+
+    def forward(self, x):
+        return x
+
+    def layers(self):
+        return self.layer
+
+def generate_conv_blocks(feat1, n, first=False):
     '''
     inputs - Number of feature maps sent into the block
     feat1 - Number of output feature2 for 1x1 Conv
@@ -38,26 +67,38 @@ def generate_conv_blocks(feat1, feat2, n, ind):
         blocks: module list of sequentials
         filters: list of output filter sizes for each stage
     '''
-    blocks = nn.ModuleList()
-    input_1 = feat2
-    input_2 = 2 * feat2 #Doubled due to skip layer concatenating two outputs
+    count = 0
+    feat2 = 2 * feat1
+    blocks = nn.ModuleList() 
     filters = []
+
+    block = nn.Sequential()
+    block.add_module(f'conv3x3', nn.Conv2d(feat1, feat2, 3, 2, 1))
+    block.add_module(f'BN', nn.BatchNorm2d(feat2))
+    block.add_module(f'Leaky', nn.LeakyReLU())
+    blocks.add_module(f'{count}', module=block)
+    count += 1
+    filters.append(feat2)
+
     for i in range(n):
         block = nn.Sequential()
-        block.add_module(f'conv1x1_{i + ind}', nn.Conv2d(input_1 if i == 0 else input_2, feat1, 1, 1, 0))
-        block.add_module(f'BN_{i + ind}', nn.BatchNorm2d(feat1))
-        block.add_module(f'Leaky_{i + ind}', nn.LeakyReLU())
-        blocks.add_module('block', module=block)
+        block.add_module(f'conv1x1', nn.Conv2d(feat2, feat1, 1, 1, 0))
+        block.add_module(f'BN_{i}', nn.BatchNorm2d(feat1))
+        block.add_module(f'Leaky_{i}', nn.LeakyReLU())
+        blocks.add_module(f'{count}', module=block)
+        count += 1
         filters.append(feat1)
         
         block = nn.Sequential()
-        block.add_module(f'conv3x3_{i + ind}', nn.Conv2d(feat1, feat2, 3, 1, 1))
-        block.add_module(f'BN_{i + ind}', nn.BatchNorm2d(feat2))
-        block.add_module(f'Leaky_{i + ind}', nn.LeakyReLU())
-        blocks.add_module('block', module=block)
+        block.add_module(f'conv3x3_{i}', nn.Conv2d(feat1, feat2, 3, 1, 1))
+        block.add_module(f'BN_{i }', nn.BatchNorm2d(feat2))
+        block.add_module(f'Leaky_{i }', nn.LeakyReLU())
+        blocks.add_module(f'{count}', module=block)
+        count += 1
         filters.append(feat2)
         
-        blocks.add_module(f'Shortcut_{i + ind}', Skip())
+        blocks.add_module(f'{count}', Skip(-3))
+        count += 1
         filters.append(2 * feat2)
     
     return blocks, np.array(filters)
@@ -67,85 +108,216 @@ def generate_pool_blocks(feat1, feat2, mask):
     feat1 - number of filters outputted by 1x1 conv
     feat2 - number of filters outputted by 3x3 conv
     mask - mask indices for detection layer
+    -------------
+    Returns:
+        blocks: module list of sequentials
+        filters: list of output filter sizes for each stage
     '''
+    count = 0
     blocks = nn.ModuleList()
     filters = []
     anchors = [[10,13], [16,30], [33,23], [30,61], [62,45], [59,119], [116,90], [156,198], [373,326]]
 
     for i in range(3):
         block = nn.Sequential()
-        block.add_module(f'conv1x1_{i}', nn.Conv2d(feat2 if i != 0 else 2 * feat2, feat1, 1, 1, 0))
+        block.add_module(f'conv1x1_{i}', nn.Conv2d(feat2, feat1, 1, 1, 0))
         block.add_module(f'BN_{i}', nn.BatchNorm2d(feat1))
         block.add_module(f'LeakyReLU', nn.LeakyReLU())
-        blocks.add_module('block', module=block)
+        blocks.add_module(f'{count}', module=block)
+        count += 1
         filters.append(feat1)
 
         block = nn.Sequential()
         block.add_module(f'conv3x3_{i}', nn.Conv2d(feat1, feat2, 3, 1, 1))
         block.add_module(f'BN_{i}', nn.BatchNorm2d(feat2))
         block.add_module(f'LeakyReLU', nn.LeakyReLU())
-        blocks.add_module('block', module=block)
+        blocks.add_module(f'{count}', module=block)
+        count += 1
         filters.append(feat2)
 
     block = nn.Sequential()
     block.add_module(f'conv1x1_{i}', nn.Conv2d(feat2, int(feat1 / 2) - 1, 1, 1, 0))
     block.add_module(f'ReLU', nn.ReLU())
-    blocks.add_module('block', module=block)   
+    blocks.add_module(f'{count}', module=block)   
+    count += 1
     filters.append((feat1 / 2) - 1)
 
 
     # YOLO layer
     anchors = [anchors[i] for i in mask]
     detection = DetectionLayer(anchors)
-    blocks.add_module(f'Detection_{i}', detection)
+    blocks.add_module(f'{count}', detection)
         
     return blocks, filters
     
+def prediction_transforms(pred, inp, anchors, classes, gpu=False):
+    '''
+    Prediction transformation taken from 
+    https://blog.paperspace.com/how-to-implement-a-yolo-v3-object-detector-from-scratch-in-pytorch-part-3/
+    '''
+    batch = pred.size(0)
+    stride = inp // pred.size(2)
+    grids = inp // stride
+    bbox = 5 + classes
+    num_anchors = len(anchors)
+
+    prediction = pred.view(batch, bbox * num_anchors, grids * grids)
+    prediction = prediction.transpose(1,2).contiguous()
+    prediction = prediction.view(batch, grids * grids * num_anchors, bbox)
+
+    anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
+
+    prediction[:, :, 0] = torch.sigmoid(prediction[:, :, 0])
+    prediction[:, :, 1] = torch.sigmoid(prediction[:, :, 1])
+    prediction[:, :, 4] = torch.sigmoid(prediction[:, :, 4])
+
+    grid = np.arange(grids)
+    a, b = np.meshgrid(grid, grid)
+
+    x_off = torch.FloatTensor(a).view(-1,1)
+    y_off = torch.FloatTensor(b).view(-1,1)
+
+    if gpu:
+        x_off = x_off.cuda()
+        y_off = y_off.cuda()
+        
+    
+    xy_off = torch.cat((x_off, y_off), 1).repeat(1, num_anchors).view(-1, 2).unsqueeze(0)
+
+
+    prediction[:, :, :2] += xy_off
+
+    anchors = torch.FloatTensor(anchors)
+    if gpu:
+        anchors = anchors.cuda()
+    anchors = anchors.repeat(grids*grids, 1).unsqueeze(0)
+
+    prediction[:, :, 2:4] = torch.exp(prediction[:, :, 2:4]) * anchors
+    prediction[: ,:, 5:5+classes] = torch.sigmoid((prediction[: ,:, 5:5+classes]))
+    prediction[:, :, :4] *= stride
+
+    return prediction
 
 class YOLO(nn.Module):
     def __init__(self):
         super(YOLO, self).__init__()
+        self.batch=64
+        self.subdivisions=16
+        self.width=256 #608
+        self.height=256
+        self.channels=3
+        self.learning_rate=0.001
         self.filters = np.array([0])
-        
+        self.mod_count = 0
         # intial convolutions
         self.features = nn.ModuleList()
         block = nn.Sequential()
         block.add_module('conv_0', nn.Conv2d(3, 32, 3, stride=1, padding=1, bias=False))
         block.add_module('batch_norm_0', nn.BatchNorm2d(32))
         block.add_module('leaky_0', nn.LeakyReLU())
-        self.features.add_module('block', module=block)
+        self.features.add_module(f'{self.mod_count}', module=block)
+        self.mod_count += 1
 
         for i, n in enumerate([1,2,8,8,4]):
-            inputs = 64 * (2**n)
+            inputs = 32 * (2**i)
+            print(inputs)
             #BLOCK 
-            blocks, f = generate_conv_blocks(inputs, 64, n, i)
+            blocks, f = generate_conv_blocks(inputs, n, True if i == 0 else False)
             self.filters = np.concatenate((self.filters, f))
-            self.features.extend(blocks)
+            #self.features.extend(blocks)
+            for mod in blocks:
+                self.features.add_module(f'{self.mod_count}', mod)
+                self.mod_count += 1
 
             block = nn.Sequential()
             block.add_module(f'conv_{i+1}', nn.Conv2d(inputs if n != 1 else int(inputs/2), 64, 3, stride=2, padding=1, bias=False))
             block.add_module(f'batch_norm_{i+1}', nn.BatchNorm2d(364))
             block.add_module(f'leaky_{i+1}', nn.LeakyReLU())
-            self.features.add_module('block', module=block)
+            self.features.add_module(f'{self.mod_count}', module=block)
 
         ########### AvgPool - Connected - Softmax ##############
 
             
-        for i, mask in enumerate([[6,7,8], [3,4,5], [0,1,2]]):
-
+        for i, (mask, route) in enumerate(zip([[6,7,8], [3,4,5], [0,1,2]], [61, 36, None])):
             blocks, f = generate_pool_blocks(128 * (2 ** (2-i)), 256 * (2 ** (2-i)), mask)
             self.filters = np.concatenate((self.features, f))
-            self.features.extend(blocks)
+            #self.features.extend(blocks)
+            for mod in blocks:
+                self.features.add_module(f'{self.mod_count}', mod)
+                self.mod_count += 1
 
             if i != 2:
-                feat1 = 128 * (2 ** (2-i)) - 1
-                self.features.add_module(f'Route_{i}', Skip())
-                self.features.add_module(f'Upsample_{i}', nn.Upsample(scale_factor=2, mode='bilinear'))
-                self.features.add_module(f'Conv1x1_{i}', nn.Conv2d(int(feat1 / 2) - 1, int(feat1/2), 1, 1, 0))
-                self.features.add_module(f'Route_{i}', Skip())
+                feat1 = 128 * (2 ** (2-i))
+                self.features.add_module(f'{self.mod_count}', Route([-4, 0]))
+                self.mod_count += 1
+                self.features.add_module(f'{self.mod_count}', nn.Upsample(scale_factor=2, mode='bilinear'))
+                self.mod_count += 1
+                self.features.add_module(f'{self.mod_count}', nn.Sequential(nn.Conv2d(feat1, int(feat1/2), 1, 1, 0),
+                                                                nn.BatchNorm2d(int(feat1/2)),
+                                                                nn.LeakyReLU()))
+                self.mod_count += 1
+                self.features.add_module(f'{self.mod_count}', Route([-1, route]))
+                self.mod_count += 1
+
+        self.filters = self.filters[1:] #Remove unnecessary first 0
+        #for name, module in self.features.named_children():
+        #    print(name, module)
+
+    def forward(self, x, gpu=False):
+        outputs = {}
+        mods = self.features
+        write = 0
+        for i, mod in enumerate(mods):
+            print(i, x.shape, type(mod))
+            if isinstance(mod, nn.Sequential):
+                x = self.features[i](x)
+
+            elif isinstance(mod, nn.Upsample):
+                x = mod(x)
+
+            elif isinstance(mod, Skip):
+                from_ = int(mod.skips())
+                x = outputs[i-1] + outputs[i+from_]
+            elif isinstance(mod, Route):
+                layers = mod.layers()
+                if layers[0] > 0:
+                    layers[0] = layers[0] - i
+                
+
+                if layers[1] == 0:
+                    print(x.shape)
+                    x = outputs[i + (layers[0])]
+                    print(x.shape)
+
+                else:
+                    if (layers[1]) > 0:
+                        layers[1] = layers[1] - i
+
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+
+                    x = torch.cat((map1, map2), 1)
+                
+            elif isinstance(mod, DetectionLayer):
+                anchors = mod.anchors()
+                inp = 256
+                classes = 80
+                x = x.data
+                x = prediction_transforms(x, inp, anchors, classes, gpu)
+                if not write:
+                    detections = x
+                    write = 1
+                else:
+                    detections = torch.cat((detections, x), 1)
+
+            else:
+                print('Unknown module!!!')
             
-    def forward(self, x):
-        
-        return x
+            outputs[i] = x
+
+        return detections
 
 net = YOLO()
+inp = get_test_input()
+pred = net(inp, False)
+print(pred)
