ebb24a02bb83e2b7252b60f0e67c75cd9b0e66b2
catarinabarino <catarinabarino@gmail.com>
2022-01-03 18:18:24 -0500

    adding rcnn training code

---
+++ Coursework2/networks/Half_U_net.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules import module
+from torch.nn.modules.activation import ReLU
+from torch.nn.modules.batchnorm import BatchNorm2d
+from torch.nn.modules.conv import Conv2d
+import torchvision
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+
+
+# higher level than tensor flow
+class Half_Unet(nn.Module):
+    def __init__(self,k=32,in_chns=3,n_segments=2):
+        """Class to define half U-net architecture
+        ----Args-----
+        k: number of output channels from first layer, which doubles after each block
+        in_chns = number of channels in input image
+        n_segments = number of output channels for segmentation
+        """
+
+        super(Half_Unet,self).__init__()
+        
+        # 1st convolutional block
+        # use padding = 1 to keep image the same size
+        # use batch normalisation and Relu at each step
+        # increase channels (3 -> k)
+        # im size = 256x256
+        self.conv1 = nn.Sequential(BatchNorm2d(in_chns),ReLU(inplace=True),Conv2d(in_chns,k,kernel_size=3,stride=1,padding=1)
+                                    ,BatchNorm2d(k),ReLU(inplace=True),Conv2d(k,k,kernel_size=3,stride=1,padding=1))
+        self.maxpool1= nn.MaxPool2d(kernel_size=2,stride=2,padding=0)
+
+        # im size = 128x128
+        # 2nd conv block, double output channels (k -> 2k)
+        self.conv2 = nn.Sequential(BatchNorm2d(k),ReLU(inplace=True),Conv2d(k,2*k,kernel_size=3,stride=1,padding=1)
+                                    ,BatchNorm2d(2*k),ReLU(inplace=True),Conv2d(2*k,2*k,kernel_size=3,stride=1,padding=1))
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2,stride=2,padding=0)
+
+        # im size = 64 x 64
+        # 3rd conv block, double output channels (2k -> 4k)
+        self.conv3 = nn.Sequential(BatchNorm2d(2*k),ReLU(inplace=True),Conv2d(2*k,4*k,kernel_size=3,stride=1,padding=1)
+                                    ,BatchNorm2d(4*k),ReLU(inplace=True),Conv2d(4*k,4*k,kernel_size=3,stride=1,padding=1))
+        #self.maxpool3 = nn.MaxPool2d(kernel_size=2,stride=2,padding=0)
+
+    def forward(self,x):
+        x = x.double()
+        #1st convolutional block
+        out1 = self.conv1(x)
+        maxpool1 = self.maxpool1(out1)
+        #2nd convolutional block
+        out2 = self.conv2(maxpool1)
+        maxpool2 = self.maxpool2(out2)
+        #3rd convolutional block
+        out3 = self.conv3(maxpool2)
+        #maxpool3 = self.maxpool3(out3)
+        return(out3)
+
+
+
+k = 4
+# load a pre-trained model for classification and return
+# only the features
+backbone = Half_Unet(k)
+backbone.out_channels = k*4
+
+# anchor generator
+anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),), aspect_ratios=((0.5, 1.0, 2.0),))
+
+# feature maps for ROI cropping and ROI sizes 
+roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'], output_size=7, sampling_ratio=2)
+
+model = FasterRCNN(backbone, num_classes=2, rpn_anchor_generator=anchor_generator, box_roi_pool=roi_pooler)
+print(model)
---
+++ Coursework2/scripts/R-CNN_training.py
@@ -0,0 +1,73 @@
+import numpy as np
+import torch
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import time
+import sys
+import os
+sys.path.insert(1, '..') # add folder above to path for easy import 
+import data_pipeline.DataUtils as DataUtils
+import data_pipeline.DatasetClass as DatasetClass
+from networks.Half_U_net import *
+
+BATCH = 16
+LR = 0.01
+MOM = 0.9
+EPOCHS = 1
+k = 4
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+data = DatasetClass.PetSegmentationDataSet('train', 'bbox', 'bin')
+dataloader = DataLoader(data, BATCH, shuffle=True)
+
+
+backbone = Half_Unet(k).to(device)
+backbone.out_channels = k*4
+# anchor generator
+anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),), aspect_ratios=((0.5, 1.0, 2.0),))
+# feature maps for ROI cropping and ROI sizes 
+roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'], output_size=7, sampling_ratio=2)
+net = FasterRCNN(backbone, num_classes=2, rpn_anchor_generator=anchor_generator, box_roi_pool=roi_pooler)
+net = net.double()
+criterion = optim.SGD(net.parameters(), LR, MOM) # optimizer
+#loss = torch.nn.MSELoss()
+
+lr_scheduler = torch.optim.lr_scheduler.StepLR(criterion, step_size=3, gamma=0.1) #learning rate scheduler
+
+losses = []
+
+for epoch in range(EPOCHS):
+    for i, data in enumerate(dataloader):
+        ims, labels, boxes = data.values()
+        labels = labels.to(torch.int64)
+
+        
+        ims = list(image for image in ims)
+        labels = labels.to(device)
+        boxes = boxes.to(device)
+        targets = [{'boxes': boxes[i].reshape((1,4)), 'labels': labels[i].reshape(1)} for i in range(len(labels))]
+
+        try:
+            output = net(ims, targets) # losses 
+
+        # except value error in case bbox values = 0
+        except ValueError as e:
+            print(e)
+            print('skipping this batch...')
+
+        else:
+            l = sum(loss for loss in output.values()) # sum loss values
+            criterion.zero_grad()
+            l.backward()
+            criterion.step()
+            losses.append(l.item())
+            print(f'Batch {i}/{len(dataloader)}')
+            print('Average Batch Loss: ', l.item())
+
+            #if (i % 5) == 0:
+            #    print(pred[0] * 256)
+            #    print(boxes[0])
+            lr_scheduler.step()
+        
+torch.save(net.state_dict(), 'rcnn.pt')
