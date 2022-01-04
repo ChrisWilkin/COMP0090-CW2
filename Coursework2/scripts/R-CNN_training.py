d5dde654573814ea86db6200d174eaf1569b19fa
Chris <ucabilk@ucl.ac.uk>
2022-01-04 14:56:58 +0000

    larger batch averages

--- Coursework2/scripts/R-CNN_training.py
+++ Coursework2/scripts/R-CNN_training.py
@@ -13,9 +13,9 @@ import data_pipeline.DatasetClass as DatasetClass
 from networks.Half_U_net import Half_Unet
 
 BATCH = 8
-LR = 0.001
+LR = 0.05
 MOM = 0.9
-EPOCHS = 5
+EPOCHS = 1
 k = 4
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -68,15 +68,15 @@ for epoch in range(EPOCHS):
             l.backward()
             criterion.step()
             losses.append(l.item())
-            print(f'Batch {i}/{len(dataloader)}')
-            print('Average Batch Loss: ', l.item())
 
             #if (i % 5) == 0:
             #    print(pred[0] * 256)
             #    print(boxes[0])
             lr_scheduler.step()
-            if (i+1) % 50 == 0:
-                print(f'50 Batches: {time.time() - t:.2f}s')
+            if (i+1) % 25 == 0:
+                print(f'Batch {i}/{len(dataloader)}')
+                print('Average 25 Batch Loss: ', np.average(losses[-25:]))
+                print(f'25 Batches: {time.time() - t:.2f}s')
                 t = time.time()
         
-torch.save(net.state_dict(), os.path.dirname(__file__)[:-len('/scripts')]+'/networks/Weights/RCNNk4lr01ep1v1.pt')
+torch.save(net.state_dict(), os.path.dirname(__file__)[:-len('/scripts')]+'/networks/Weights/RCNNk4lr05ep1v1.pt')
