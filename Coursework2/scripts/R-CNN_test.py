54048d099df255cf5da127e2e2a4f0181a365847
Chris <ucabilk@ucl.ac.uk>
2022-01-04 12:47:31 +0000

    test file done

---
+++ Coursework2/scripts/R-CNN_test.py
@@ -0,0 +1,89 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torchvision 
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+import sys
+import os
+sys.path.append(os.path.dirname(__file__)[:-len('/scripts')])
+import data_pipeline.DataUtils as DataUtils
+import data_pipeline.DatasetClass as DatasetClass
+from networks.Half_U_net import Half_Unet
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+dataset = DatasetClass.PetSegmentationDataSet('test')
+dataloader = DataLoader(dataset, batch_size=10, shuffle=True, num_workers=0)
+del dataset
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Using ', device)
+
+k=4
+
+backbone = Half_Unet(k).to(device)
+backbone.out_channels = k*4
+# anchor generator
+anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),), aspect_ratios=((0.5, 1.0, 2.0),))
+# feature maps for ROI cropping and ROI sizes 
+roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'], output_size=7, sampling_ratio=2)
+net = FasterRCNN(backbone, num_classes=2, rpn_anchor_generator=anchor_generator, box_roi_pool=roi_pooler).to(device)
+net = net.double()
+
+net.load_state_dict(torch.load(os.path.dirname(__file__)[:-len('/scripts')]+'/networks/Weights/RCNNk4lr01ep5v1.pt', map_location=device))
+
+net.eval()
+
+losses = []
+
+selection = np.random.randint(0, len(dataloader))
+
+detection_threshold = 0.35
+
+CLASSES = ['Cat', 'Dog']
+
+
+for j, ims in enumerate(dataloader):
+    if j == 0:
+        for i , orig_image in enumerate(ims['images']):
+            image_name = f'Image {i}'
+            image = orig_image
+            image /= 255
+            image = torch.unsqueeze(orig_image, 0)
+            
+            with torch.no_grad():
+                outputs = net(image.to(device)) # losses 
+            outputs = [{k: v.to('cpu') for k, v in t.items()} for t in outputs]
+
+            orig_image = np.transpose(np.squeeze(image.detach().cpu().numpy()), (1,2,0))
+
+            if len(outputs[0]['boxes']) != 0:
+                boxes = outputs[0]['boxes'].data.numpy()
+            scores = outputs[0]['scores'].data.numpy()
+            # filter out boxes according to `detection_threshold`
+            boxes = boxes[scores >= detection_threshold].astype(np.int32)
+            draw_boxes = boxes.copy()
+            # get all the predicited class names
+            pred_classes = [CLASSES[i] for i in outputs[0]['labels'].cpu().numpy()]
+            
+            # draw the bounding boxes and write the class name on top of it
+            for k, box in enumerate(draw_boxes):
+                print(box, pred_classes[k], scores[k], i)
+                cv2.rectangle(orig_image,
+                            (int(box[0]), int(box[1])),
+                            (int(box[2]), int(box[3])),
+                            (0, 0, 255), 2)
+                cv2.putText(orig_image, pred_classes[j], 
+                            (int(box[0]), int(box[1]-5)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 
+                            2, lineType=cv2.LINE_AA)
+            cv2.imshow('Prediction', orig_image)
+            cv2.waitKey(10)
+            cv2.imwrite(f"../test_predictions/{image_name}.jpg", orig_image,)
+        print(f"Image {i+1} done...")
+        print('-'*50)
+
+plt.figure(1)
+plt.subplot(111)
+plt.plot(losses)
--- Coursework2/scripts/R-CNN_training.py
+++ Coursework2/scripts/R-CNN_training.py
@@ -2,24 +2,28 @@ import numpy as np
 import torch
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
+import torchvision 
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
 import time
 import sys
 import os
 sys.path.append(os.path.dirname(__file__)[:-len('/scripts')])
-import data_pipeline.DataUtils as DataUtils
 import data_pipeline.DatasetClass as DatasetClass
-from networks.Half_U_net import *
+from networks.Half_U_net import Half_Unet
 
-BATCH = 16
-LR = 0.01
+BATCH = 8
+LR = 0.001
 MOM = 0.9
-EPOCHS = 1
+EPOCHS = 5
 k = 4
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Using ', device)
 
 data = DatasetClass.PetSegmentationDataSet('train', 'bbox', 'bin')
 dataloader = DataLoader(data, BATCH, shuffle=True)
+del data
 
 
 backbone = Half_Unet(k).to(device)
@@ -28,8 +32,10 @@ backbone.out_channels = k*4
 anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),), aspect_ratios=((0.5, 1.0, 2.0),))
 # feature maps for ROI cropping and ROI sizes 
 roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'], output_size=7, sampling_ratio=2)
-net = FasterRCNN(backbone, num_classes=2, rpn_anchor_generator=anchor_generator, box_roi_pool=roi_pooler)
+net = FasterRCNN(backbone, num_classes=2, rpn_anchor_generator=anchor_generator, box_roi_pool=roi_pooler).to(device)
 net = net.double()
+torch.save(net.state_dict(), os.path.dirname(__file__)[:-len('/scripts')]+'/networks/Weights/RCNNk4lr01ep1v1.pt')
+
 criterion = optim.SGD(net.parameters(), LR, MOM) # optimizer
 #loss = torch.nn.MSELoss()
 
@@ -38,15 +44,14 @@ lr_scheduler = torch.optim.lr_scheduler.StepLR(criterion, step_size=3, gamma=0.1
 losses = []
 
 for epoch in range(EPOCHS):
+    t = time.time()
     for i, data in enumerate(dataloader):
         ims, labels, boxes = data.values()
         labels = labels.to(torch.int64)
-
         
-        ims = list(image for image in ims)
+        ims = list(image.to(device) for image in ims)
         labels = labels.to(device)
         boxes = boxes.to(device)
-        ims = ims.to(device)
         targets = [{'boxes': boxes[i].reshape((1,4)), 'labels': labels[i].reshape(1)} for i in range(len(labels))]
 
         try:
@@ -70,5 +75,8 @@ for epoch in range(EPOCHS):
             #    print(pred[0] * 256)
             #    print(boxes[0])
             lr_scheduler.step()
+            if (i+1) % 50 == 0:
+                print(f'50 Batches: {time.time() - t:.2f}s')
+                t = time.time()
         
-torch.save(net.state_dict(), os.path.dirname(__file__)+'/networks/Weights/RCNNk4lr01ep1v1.pt')
+torch.save(net.state_dict(), os.path.dirname(__file__)[:-len('/scripts')]+'/networks/Weights/RCNNk4lr01ep1v1.pt')
