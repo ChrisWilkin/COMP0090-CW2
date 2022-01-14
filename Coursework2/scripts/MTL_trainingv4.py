21e38249d1b9ef2afe63e234f1beddc9a99689ff
Sachin Beepath <sachin.beepath@gmail.com>
2022-01-14 16:44:40 +0000

    added random weights to arg parse

--- Coursework2/scripts/MTL_trainingv4.py
+++ Coursework2/scripts/MTL_trainingv4.py
@@ -34,8 +34,9 @@ parser.add_argument("--body", help="filename for storing state dict of MTL body 
 parser.add_argument("--seg", help="filename for storing state dict of segmentation after training", type=str, default="MTL_Seg.pt")
 parser.add_argument("--r", help="filename for storing state dict of ROI after training", type=str, default="MTL_ROI.pt")
 parser.add_argument("--epochs", help="number of training epochs", type=int, default=10)
-parser.add_argument("--rand-task", help="Randomly choose which task to update each batch", type=int, default=False)
-
+parser.add_argument('--task', help="does not randomly choose which task to update each batch", dest='rand_weights', action='store_false')
+parser.add_argument('--rand-task', help="randomly choose which task to update each batch", dest='rand_weights', action='store_true')
+parser.set_defaults(task=False)
 args = parser.parse_args()
 
 inc_roi = args.inc_roi
@@ -115,7 +116,7 @@ total_losses = []
 seg_accuracy = []
 cls_accuracy = []
 
-RANDOM_WEIGHTS = args.rand_task  #Trigger random optimiser steps (randomly choose which task to update each  batch)
+RANDOM_WEIGHTS = args.rand_weights  #Trigger random optimiser steps (randomly choose which task to update each  batch)
 ROI_UPDATE = inc_roi   #ROI
 SEG_UPDATE = True
 
