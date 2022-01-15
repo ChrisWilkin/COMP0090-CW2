5bb578fb9beeb025999b77e0199517b56fcdaa68
sachinbeepath <42979544+sachinbeepath@users.noreply.github.com>
2022-01-15 21:03:48 +0000

    Add files via upload

---
+++ submission/Coursework2/data_pipeline/DataPreprocessing.py
@@ -0,0 +1,312 @@
+import h5py as h5
+import os
+import numpy as np
+from PIL import Image, ImageDraw
+import math
+from numpy.core.defchararray import add
+from numpy.core.fromnumeric import resize
+from numpy.testing._private.utils import print_assert_equal
+from xml.dom import minidom
+from DataUtils import paths, add_data_to_h5
+
+
+PATH_OG = f'{os.path.dirname(__file__)[:-14]}/Datasets/CompleteDataset'
+
+def add_margin(img, top, right, bottom, left, color):
+    '''
+    adds black margin with thicknesses specified for each side
+    '''
+
+    width, height = img.size
+    new_width = width + right + left
+    new_height = height + top + bottom
+    result = Image.new(img.mode, (new_width, new_height), color)
+    result.paste(img, (left, top))
+    return result
+
+def crop_image(img, height, width, png=False):
+    '''
+    crops input image(PIL Image) to size height x width.
+    If image is smaller than these dimensions, it is padded with black (zeros)
+    '''
+    if img.size[0] < width or img.size[1] < height:
+        w_diff = width - img.size[0]
+        h_diff = height - img.size[1]
+        w_diff = (np.abs(w_diff) + w_diff) / 2 # zero if wdiff -ve , unchanged if wdiff +ve
+        h_diff = (np.abs(h_diff) + h_diff) / 2    
+        right = math.ceil(w_diff / 2) 
+        left = math.floor(w_diff / 2)
+        top = math.ceil(h_diff / 2) 
+        bottom = math.floor(h_diff / 2)
+        if png:
+            img = add_margin(img, top, right, bottom, left, 2)
+        else:
+            img = add_margin(img, top, right, bottom, left, (0, 0, 0))
+    
+    w_diff = img.size[0] - width
+    h_diff = img.size[1] - height  
+    right = img.size[0] - math.ceil(w_diff / 2) 
+    left = math.floor(w_diff / 2)
+    top = math.ceil(h_diff / 2) 
+    bottom = img.size[1] - math.floor(h_diff / 2)
+    img = img.crop((left, top, right, bottom))
+
+    return img
+
+def array_from_jpg(file_name, crop=None, resize=None):
+    '''
+    Opens a .jpg file and returns a numpy array of H x W x C
+    '''
+    file = Image.open(file_name)
+    if crop is not None:
+        file = crop_image(file, crop[0], crop[1])
+    if resize is not None:
+        if crop is None:
+            file = crop_image(file, np.max(file.size), np.max(file.size)) #Pad to square
+        file = file.resize((resize[0], resize[1]))
+
+    try:
+        pix = np.array(file.getdata()).reshape(file.size[0], file.size[1], 3).astype(np.uint8)
+    except:
+        print(f'Failed to convert {file_name} to array!')
+        pix = np.zeros((256, 256, 3)).astype(np.uint8)
+    file.close()
+    return pix
+
+
+def array_from_png(file_name, crop=None, resize=None):
+    '''
+    converts png file to numpy array, with cropping / padding
+    '''
+    file = Image.open(file_name)
+
+    if crop is not None:
+        file = crop_image(file, crop[0], crop[1])
+    if resize is not None:
+        if crop is None:
+            file = crop_image(file, np.max(file.size), np.max(file.size), png=True)
+        file = file.resize((resize[0], resize[1]))
+    
+    try:
+        pix = np.array(file.getdata()).reshape(file.size[0], file.size[1]).astype(np.uint8)
+    except:
+        pix = np.zeros((256, 256, 1)).astype(np.uint8)
+        print(f'array_from_png failed to convert {file_name} to array!')
+
+    file.close()
+
+    return pix
+
+def coords_from_xml(file, crop, resize):
+    '''
+    extracts bounding box coordinates from xml file and accounts for image cropping
+    '''
+    f = minidom.parse(file, )
+    xmin = int(f.getElementsByTagName('xmin')[0].firstChild.data)
+    xmax = int(f.getElementsByTagName('xmax')[0].firstChild.data)
+    ymin = int(f.getElementsByTagName('ymin')[0].firstChild.data)
+    ymax = int(f.getElementsByTagName('ymax')[0].firstChild.data)
+    width = int(f.getElementsByTagName('width')[0].firstChild.data)
+    height = int(f.getElementsByTagName('height')[0].firstChild.data)
+
+    if crop is not None:
+        #print(height, crop[0])
+        h_diff = height - crop[0]
+        w_diff = width - crop[1]
+        ymin = 0 if ymin < h_diff / 2 else ymin - np.floor(h_diff/2)
+        ymax = crop[0] if ymax - crop[0] > h_diff / 2 else ymax - np.floor(h_diff/2)
+        xmin = 0 if xmin < w_diff / 2 else xmin - np.floor(w_diff/2)
+        xmax = crop[1] if xmax - crop[1] > w_diff / 2 else xmax - np.floor(w_diff/2)
+    if resize is not None:
+        if crop is None:
+            crop = np.array([max(width, height), max(width, height)])
+            h_diff = height - crop[0]
+            w_diff = width - crop[1]
+            ymin = 0 if ymin < h_diff / 2 else ymin - np.floor(h_diff/2)
+            ymax = crop[0] if ymax - crop[0] > h_diff / 2 else ymax - np.floor(h_diff/2)
+            xmin = 0 if xmin < w_diff / 2 else xmin - np.floor(w_diff/2)
+            xmax = crop[1] if xmax - crop[1] > w_diff / 2 else xmax - np.floor(w_diff/2)
+        xmin = int(xmin * (resize[1] / crop[1]))
+        ymin = int(ymin * (resize[0] / crop[0]))
+        xmax = int(xmax * (resize[1] / crop[1]))
+        ymax = int(ymax * (resize[0] / crop[0]))
+
+    return (xmin, ymin, xmax, ymax)
+
+def get_files(path, extension=None, ind=None, names=None):
+    '''
+    Give a path to a folder, a file extension to check for, and an optional list of indices, 
+    this returns the name of all files in the folder.
+    '''
+    ttv = {'train': 0.6, 'test': 0.2, 'val': 0.2}
+
+    file_names = np.array(next(os.walk(path), (None, None, []))[2])  # list of file names in folder
+    if extension is not None:
+        exts = [file_names[i][-3:] == extension for i in range(len(file_names))] #ignore any non .jpg files
+        file_names = file_names[exts]
+    if ind is not None:
+        assert len(ind) <= len(file_names), f'Files: {len(file_names)}, Ind: {len(ind)}'
+        file_names = [f'{names[i]}.{extension}' for i in ind]   # if indices listed, apply this
+
+    return file_names
+
+def get_files_bbox(path, extension=None, ind=None, dict=None):
+    '''
+    Give a path to a folder, a file extension to check for, and an optional list of indices, 
+    this returns the name of all files in the folder.
+    '''
+    ttv = {'train': 0.6, 'test': 0.2, 'val': 0.2}
+
+    file_names = np.array(next(os.walk(path), (None, None, []))[2])  # list of file names in folder
+    if extension is not None:
+        exts = [file_names[i][-3:] == extension for i in range(len(file_names))] #ignore any non .jpg files
+        file_names = file_names[exts]
+    if ind is not None:
+        fls = []
+        for i in ind:
+            name = f'{dict[i]}.xml'
+            if name in file_names:
+                fls.append(name)
+        file_names = np.array(fls)
+
+
+    return file_names
+
+def load_data(folder, test_train_val=None, indices=None, crop_size=None, return_dicts=False, resize=None):
+    '''
+    folder: images / masks / bboxes / bins - specifies the type of data to be loaded
+    test_train_val: if set to 'train' or 'test' or 'val, uses a default 60:20:20 data split. Default None returns all data
+    indices: if None, all data returned, otherwise accepts int or array([int]) to return specific indices of data
+    crop_size: array([height, width]) to crop images to. If image is smaller, it will be padded with black
+    return_dicts: whether to return the name_id and id_name dictionaries
+    resize: dimensions to resize image to (after cropping)
+    '''
+    print(f'Beginning {folder} loading')
+    assert folder in ['images', 'masks', 'bboxes', 'bins'], 'Invalid data category. Must be in images / masks / bboxes / bins'
+    
+    path = paths(PATH_OG, 'annotations', 'list.txt')
+    file = pd.read_csv(path, sep=' ', skiprows=6, names=['Image', 'ID', 'Species', 'Breed'])
+    df = pd.DataFrame(file)
+    names, id = df['Image'], np.arange(0, len(df['Image']), 1)
+    name_id = {}
+    id_names = {}
+    for i in range(len(names)):
+        name_id[names[i]] = id[i]
+        id_names[id[i]] = names[i]
+
+    if test_train_val is not None:
+        assert indices is None, 'test_train_val and indices cannot both be specified'
+        assert test_train_val in ['test', 'train', 'val'], 'Invalid argument. Must be in test / train / val'
+        indices = []
+        if test_train_val == 'test':
+            path = paths(PATH_OG, 'annotations', 'test.txt')
+            test_file = pd.read_csv(path, sep=' ', skiprows=6, names=['Image', 'ID', 'Species', 'Breed']) 
+            test_df = pd.DataFrame(test_file)
+            for i in range(0, len(test_file['Image']), 2):
+                indices.append(name_id[test_df['Image'][i]])
+        if test_train_val == 'val':
+            path = paths(PATH_OG, 'annotations', 'test.txt')
+            test_file = pd.read_csv(path, sep=' ', skiprows=6, names=['Image', 'ID', 'Species', 'Breed']) 
+            test_df = pd.DataFrame(test_file)
+            for i in range(1, len(test_file['Image']), 2):
+                indices.append(name_id[test_df['Image'][i]])
+        if test_train_val == 'train':
+            path = paths(PATH_OG, 'annotations', 'trainval.txt')
+            test_file = pd.read_csv(path, sep=' ', skiprows=6, names=['Image', 'ID', 'Species', 'Breed']) 
+            test_df = pd.DataFrame(test_file)
+            for i in range(len(test_file['Image'])):
+                indices.append(name_id[test_df['Image'][i]])
+        indices = np.array(indices)
+
+    if indices is not None:
+        assert isinstance(indices, np.ndarray), 'Invalid indices entry. Must by numpy array of integers'
+        indices = indices.tolist()
+    
+    if crop_size is not None:
+        assert isinstance(crop_size, np.ndarray)
+        assert len(crop_size) == 2
+
+    if resize is not None:
+        assert isinstance(resize, np.ndarray)
+        assert len(resize) == 2
+
+    data = []
+    ids = []
+
+    if folder == 'images':
+        path = paths(PATH_OG, 'images') #path to images folder
+        file_names = get_files(path, 'jpg', indices, id_names)
+
+        for i, name in enumerate(file_names):
+            data.append(array_from_jpg(paths(path, name), crop_size, resize))  # load numpy array in for every filename
+            ids.append(name_id[name[:-4]])
+            if (i+1) % 250 == 0:
+                print(f'Loaded {i+1} / {len(file_names)}')
+            # array is in WxHxC format
+
+    elif folder == 'masks':
+        path = paths(PATH_OG, 'annotations', 'trimaps')
+        file_names = get_files(path, 'png', indices, id_names)
+        for i, name in enumerate(file_names):
+            data.append(array_from_png(paths(path, name), crop_size, resize))
+            ids.append(name_id[name[:-4]])
+            if (i+1) % 250 == 0:
+                print(f'Loaded {i+1} / {len(file_names)}')
+            # array is in WxHxC format
+
+    elif folder == 'bboxes':
+        path = paths(PATH_OG, 'annotations', 'xmls')
+        file_names = get_files_bbox(path, 'xml', indices, id_names)
+        for i, name in enumerate(file_names):
+            data.append(coords_from_xml(paths(path, name), crop_size, resize))
+            ids.append(name_id[name[:-4]])
+            if (i+1) % 250 == 0:
+                print(f'Loaded {i+1} / {len(file_names)}')
+            # array is in WxHxC format
+
+    elif folder == 'bins':
+        path = paths(PATH_OG, 'annotations', 'list.txt')
+        file = pd.read_csv(path, sep=' ', skiprows=6, names=['Image', 'ID', 'Species', 'Breed'])
+        df = pd.DataFrame(file)
+        if indices is not None:
+            for i in indices:
+                data.append([df.iloc[i]['Species'] - 1, df.iloc[i]['Breed']])
+                ids.append(name_id[df.iloc[i]['Image']])
+        else:
+            for i in range(len(df['ID'])):
+                data.append([df.iloc[i]['Species'] - 1, df.iloc[i]['Breed'], name_id[df.iloc[i]['Image']]])
+                ids.append(name_id[df.iloc[i]['Image']])
+    print(f'{folder} loaded successfully')   
+    print('')
+    if data == []:
+        data = [[0]]
+
+    if return_dicts:
+        return np.array(data), np.array(ids), name_id, id_names
+    else:
+        return np.array(data), np.array(ids)
+
+
+
+
+#############################################################################################
+######################## Apply Preprocessing and Create h5 File #############################
+############################################################################################
+
+#file = paths(PATH_OG, 'AllData.h5')
+#print(file)
+#f = h5.File(file, 'w')
+#train = f.create_group('train')
+#test = f.create_group('test')
+#val = f.create_group('val')
+#
+#for cat in ['test', 'train', 'val']:
+#    for label in ['images','bboxes', 'masks', 'bins']:
+#        data, ids = load_data(label, cat, resize=np.array([256, 256]))
+#        f.create_group(f'/{cat}/{label}')
+#        f.create_dataset(f'/{cat}/{label}/{label}', data=data)
+#        f.create_dataset(f'/{cat}/{label}/ID', data=ids)
+#        del data
+#        del ids
+#        print(f.keys())
+#f.close()
---
+++ submission/Coursework2/data_pipeline/DatasetClass.py
@@ -0,0 +1,319 @@
+'''
+Created 19:15 21/12/2021 by Christopher Wilkin
+
+Dataloader file containing all methods and functions relating to loading data...
+'''
+
+'''
+TO DO:
+1. Look at coverting each file's data to correct foramt (i.e. tensors in NHWC)
+2. add visualisation of images and labels
+3. modify def __len__() to give comprehensive description of file lengths and data shapes etc.
+4. can we combine all the h5 files into one? Is it faster?
+'''
+
+import torch
+from torch.utils.data import Dataset, DataLoader, dataloader
+import data_pipeline.DataUtils as DataUtils
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
+
+class PetSegmentationDataSet(Dataset):
+    def __init__(self, folder, *args):
+        '''
+        folder: the folder to take data from (test/train/val)
+        *args: specifies which targets to load data from (mask, bbox, bin)
+        '''
+        super().__init__()
+        self.mask = False if 'mask' not in args else True
+        self.bbox = False if 'bbox' not in args else True
+        self.bin = False if 'bin' not in args else True
+
+        assert folder in ['train', 'test', 'val'], 'Invalid folder option: must be train/test/val'
+        self.folder = folder
+        
+        self.data = self.load_data(self.mask, self.bin, self.bbox)
+        
+    def __len__(self):
+        return len(self.data['images'])
+
+    def __getitem__(self, ind):
+        if torch.is_tensor(ind):
+            ind = ind.tolist()
+        
+        sample = {}
+        for key in self.data.keys():
+            sample[key] = self.data[key][ind]
+
+        return sample
+
+    def load_data(self, mask, bin, bbox):
+        '''
+        Selectively loads data according to what labels are specified
+        '''
+        print('Loading Data...')
+        t = time.time()
+        img = DataUtils.load_data_from_h5(self.folder, 'images.h5')
+        img = torch.from_numpy(img).permute(0,3,1,2)
+        data = {'images':img}
+        
+        if mask:
+            mask = DataUtils.load_data_from_h5(self.folder, 'masks.h5')
+            mask = torch.from_numpy(mask).permute(0,3,1,2)
+            data['masks'] = mask
+        if bin:
+            bins = torch.from_numpy(DataUtils.load_data_from_h5(self.folder, 'binary.h5'))
+            bins = bins + 1 # classes are 1,2 instead of 0,1
+            data['bins'] = bins
+        if bbox:
+            box = torch.from_numpy(DataUtils.load_data_from_h5(self.folder, 'bboxes.h5'))
+            data['bbox'] = box
+        print(f'Finished Loading Data ({time.time() - t:.2f}s)')
+        return data
+    
+    def visualize_data(self):
+        index = np.random.randint(self.data['images'].size()[0]-1)
+        img = self.data['images'][index].permute(1,2,0).numpy()/255
+        fig, ax = plt.subplots()
+        if self.mask:
+            msk = self.data['masks'][index].permute(1,2,0).numpy()
+            msk = np.ones(msk.shape)-msk
+            img = img - msk
+            
+        ax.imshow(img)
+        
+        if self.bbox:
+            box = np.round(self.data['bbox'][index])
+            x = box[0]
+            y = box[1]
+            width = box[2] - box[0]
+            height = box[3] - box[1]
+            rect = patches.Rectangle((x, y), width, height, linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+
+        if self.bin:
+            label = "dog" if self.data['bins'][index] == 0 else "cat"
+            fig.text(0.25, 0.80,label,fontsize = 10,bbox ={'facecolor':'white','alpha':0.6,'pad':10})
+        
+        # remove one of these
+        fig.show()
+        plt.show()
+        
+        
+        
+class CompletePetDataSet(Dataset):
+    def __init__(self, file, group_name, *args):
+        '''
+        file: file path for h5 file
+        group_name: train / test / val
+        *args: specifies which targets to load data from (masks, bboxes, bins)
+        '''
+        super().__init__()
+        self.file = file
+        self.mask = False if 'masks' not in args else True
+        self.bbox = False if 'bboxes' not in args else True
+        self.bin = False if 'bins' not in args else True
+
+        assert group_name in ['train', 'test', 'val'], 'Invalid group option: must be train/test/val'
+        self.group_name = group_name
+        
+        self.data = self.load_data(self.mask, self.bin, self.bbox)
+        
+    def __len__(self):
+        return len(self.data['images'])
+
+    def __getitem__(self, ind):
+        if torch.is_tensor(ind):
+            ind = ind.tolist()
+        
+        sample = {}
+        for key in self.data.keys():
+            sample[key] = self.data[key][ind]
+
+        return sample
+
+    def load_data(self, mask, bin, bbox):
+        '''
+        Selectively loads data according to what labels are specified
+        '''
+        print('Loading Data...')
+        t = time.time()
+        img, img_ID = DataUtils.load_group_h5(self.file,self.group_name, 'images')
+        img = torch.from_numpy(img).permute(0,3,1,2)
+        data = {'images':img,
+                'images_ID': img_ID}
+        if mask:
+            masks, masks_ID = DataUtils.load_group_h5(self.file,self.group_name,'masks')
+            masks = torch.from_numpy(masks)
+            masks[masks==3]=1
+            masks[masks==2]=0
+            data['masks'] = masks
+            data['masks_ID'] = masks_ID
+        if bin:
+            bins, bins_ID = DataUtils.load_group_h5(self.file,self.group_name,'bins')
+            bins = torch.from_numpy(bins)
+            data['bins'] = bins
+            data['bins_ID'] = bins_ID
+            
+        if bbox:
+            bboxes, bboxes_ID = DataUtils.load_group_h5(self.file,self.group_name,'bboxes')
+            bboxes = torch.from_numpy(bboxes)
+            data['bboxes'] = bboxes
+            data['bboxes_ID'] = bboxes_ID
+        print(f'Finished Loading Data ({time.time() - t:.2f}s)')
+        return data
+    
+    def visualize_data(self):
+        if self.bbox:
+            ID = np.random.choice(self.data['bboxes_ID'])
+            image_index = np.where(self.data['images_ID'] == ID)[0][0]
+            bbox_index = np.where(self.data['bboxes_ID'] == ID)[0][0]
+        else:
+            image_index = np.random.randint(len(self.data['images_ID'])-1)
+            
+        
+        
+        img = self.data['images'][image_index].permute(1,2,0).numpy()/255
+        fig, ax = plt.subplots()
+        if self.mask:
+            mask_index = image_index
+            msk = self.data['masks'][mask_index].numpy().reshape(256,256,1)
+            msk = np.ones(msk.shape)-msk
+            img = img - msk
+            
+        ax.imshow(img)
+        
+        if self.bbox:
+            box = np.round(self.data['bboxes'][bbox_index])
+            x = box[0]
+            y = box[1]
+            width = box[2] - box[0]
+            height = box[3] - box[1]
+            rect = patches.Rectangle((x, y), width, height, linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+
+        if self.bin:
+            bin_index = image_index
+            label = "dog" if self.data['bins'][bin_index][0] == 1 else "cat"
+            fig.text(0.25, 0.80,label,fontsize = 10,bbox ={'facecolor':'white','alpha':0.6,'pad':10})
+        
+        # remove one of these
+        fig.show()
+        plt.show()
+        
+        
+class CompletePetDataSet(Dataset):
+    def __init__(self, file, group_name, *args):
+        '''
+        folder: the folder to take data from (test/train/val)
+        *args: specifies which targets to load data from (mask, bbox, bins)
+        '''
+        super().__init__()
+        self.file = file
+        self.mask = False if 'masks' not in args else True
+        self.bbox = False if 'bboxes' not in args else True
+        self.bin = False if 'bins' not in args else True
+
+        assert group_name in ['train', 'test', 'val'], 'Invalid group option: must be train/test/val'
+        self.group_name = group_name
+        
+        self.data = self.load_data(self.mask, self.bin, self.bbox)
+        
+    def __len__(self):
+        return len(self.data['images'])
+
+    def __getitem__(self, ind):
+        if torch.is_tensor(ind):
+            ind = ind.tolist()
+        
+        sample = {}
+        for key in self.data.keys():
+            sample[key] = self.data[key][ind]
+
+        return sample
+
+    def load_data(self, mask, bin, bbox):
+        '''
+        Selectively loads data according to what labels are specified
+        '''
+        print('Loading Data...')
+        t = time.time()
+        img, img_ID = DataUtils.load_group_h5(self.file,self.group_name, 'images')
+        img = torch.from_numpy(img).permute(0,3,1,2)
+        data = {'images':img,
+                'images_ID': img_ID}
+        if mask:
+            masks, masks_ID = DataUtils.load_group_h5(self.file,self.group_name,'masks')
+            masks = torch.from_numpy(masks)
+            masks[masks==3]=1
+            masks[masks==2]=0
+            data['masks'] = masks
+            data['masks_ID'] = masks_ID
+        if bin:
+            bins, bins_ID = DataUtils.load_group_h5(self.file,self.group_name,'bins')
+            bins = torch.from_numpy(bins)
+            data['bins'] = bins
+            data['bins_ID'] = bins_ID
+            
+        if bbox:
+            bboxes, bboxes_ID = DataUtils.load_group_h5(self.file,self.group_name,'bboxes')
+            bboxes = torch.from_numpy(bboxes)
+            data['bboxes'] = bboxes
+            data['bboxes_ID'] = bboxes_ID
+
+        if bbox and self.group_name == 'train': #Weird bug where bboxes have two less data points than others
+            for key in data.keys():
+                data[key] = data[key][:3666]
+
+        print(f'Finished Loading Data ({time.time() - t:.2f}s)')
+        return data
+    
+    def visualize_data(self):
+        if self.bbox:
+            ID = np.random.choice(self.data['bboxes_ID'])
+            image_index = np.where(self.data['images_ID'] == ID)[0][0]
+            bbox_index = np.where(self.data['bboxes_ID'] == ID)[0][0]
+        else:
+            image_index = np.random.randint(len(self.data['images_ID'])-1)
+            
+        
+        
+        img = self.data['images'][image_index].permute(1,2,0).numpy()/255
+        fig, ax = plt.subplots()
+        if self.mask:
+            mask_index = image_index
+            msk = self.data['masks'][mask_index].numpy().reshape(256,256,1)
+            msk = np.ones(msk.shape)-msk
+            img = img - msk
+            
+        ax.imshow(img)
+        
+        if self.bbox:
+            box = np.round(self.data['bboxes'][bbox_index])
+            x = box[0]
+            y = box[1]
+            width = box[2] - box[0]
+            height = box[3] - box[1]
+            rect = patches.Rectangle((x, y), width, height, linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+
+        if self.bin:
+            bin_index = image_index
+            label = "dog" if self.data['bins'][bin_index][0] == 1 else "cat"
+            fig.text(0.25, 0.80,label,fontsize = 10,bbox ={'facecolor':'white','alpha':0.6,'pad':10})
+        
+        # remove one of these
+        fig.show()
+        plt.show()
+        
+
+
+#dataset = PetSegmentationDataSet('test', 'mask')
+#dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=0)
+#print(dataset.__len__())
+#dataset.visualize_data()
+
+
